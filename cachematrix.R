fa386d96e0b01dd445b7112c4b99e2aa2db22a23
pgalainena <pilargalainena@gmail.com>
2020-06-19 13:21:10 +0200

    R Programming: Project Week 2

---
+++ cachematrix.R
@@ -0,0 +1,30 @@
+## Author: pgalainena
+
+## Create a special vector
+makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+        x <<- y
+        m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+}
+
+##Cache the inverse matrix
+cacheSolve <- function(x, ...) {
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
+}
